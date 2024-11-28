ba8328ba0c8db2c12ab33353f8ab78d8a8a9e145
Aniruddh Dubey <justani98@gmail.com>
2024-11-28 10:36:18 +0530

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import reactLogo from './assets/react.svg'
-import codechefLogo from './assets/codechef.svg'
-import './App.css'
-
-function App() {
+export default function App() {
     return (
-    <>
-      <div>
-        <a href="https://codechef.com" target="_blank">
-          <img src={codechefLogo} className="logo" alt="CodeChef logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CodeChef Labs</h1>
-    </>
-  )
+      <h1>Hello React</h1>
+    )
 }
-
-export default App
